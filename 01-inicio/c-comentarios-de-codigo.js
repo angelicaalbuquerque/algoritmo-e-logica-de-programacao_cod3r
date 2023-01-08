600a5df23bd35754920658f0ea23956c2db9cf8a
Angélica Albuquerque <angelica.o.albuquerque@gmail.com>
2023-01-08 12:19:19 -0300

    Comentário de código

---
+++ 01-inicio/c-comentarios-de-codigo.js
@@ -0,0 +1,4 @@
+// Comentários sejam para justificar uma escolha, desabilitar alguma sentença que não deve ser executada, ou para documentar o código.
+
+console.log("Olá");
+//console.log é usado para exibir algo na tela do computador
