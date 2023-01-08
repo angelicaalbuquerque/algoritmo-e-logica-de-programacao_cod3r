e4aa9aaaf1995d57dea942b8c81ff9a7395d3d70
Angélica Albuquerque <angelica.o.albuquerque@gmail.com>
2023-01-08 12:22:15 -0300

    Comentários de código

--- 01-inicio/c-comentarios-de-codigo.js
+++ 01-inicio/c-comentarios-de-codigo.js
@@ -1,4 +1,17 @@
-// Comentários sejam para justificar uma escolha, desabilitar alguma sentença que não deve ser executada, ou para documentar o código.
-
-console.log("Olá");
 //console.log é usado para exibir algo na tela do computador
+console.log("Olá");
+
+/* 
+Esse é um comentário de múltiplas linhas.
+
+Comentários podem ser utilizados sejam para: 
+- justificar uma escolha;
+- desabilitar alguma sentença que não deve ser executada
+- ou para documentar o código.
+*/
+
+/*
+ * Esse é um
+ * estilo de comentário
+ * muito utilizado
+ */
