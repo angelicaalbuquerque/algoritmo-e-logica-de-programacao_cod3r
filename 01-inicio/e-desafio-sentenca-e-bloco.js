4d3684a490013f085539139b8e833e1641f5d28d
Angélica Albuquerque <angelica.o.albuquerque@gmail.com>
2023-01-08 12:58:13 -0300

    Desafio 01 - sentença e bloco de código

---
+++ 01-inicio/e-desafio-sentenca-e-bloco.js
@@ -0,0 +1,27 @@
+/*
+Exibir no console os números pares e ímpares, de 1 a 10, imprimindo os ímpares em um bloco e os pares em outro bloco.
+*/
+
+// Bloco 1 - impares
+{
+  console.log(
+    "Imprimindo bloco de código com sentenças de código com valores impares:",
+  );
+  console.log(1);
+  console.log(3);
+  console.log(5);
+  console.log(7);
+  console.log(9);
+}
+
+// Bloco 2 - pares
+{
+  console.log(
+    "Imprimindo bloco de código com sentenças de código com valores pares:",
+  );
+  console.log(2);
+  console.log(4);
+  console.log(6);
+  console.log(8);
+  console.log(10);
+}
