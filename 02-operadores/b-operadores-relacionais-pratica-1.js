05e4219cce1999f8aa7ac9a5dbf504df9c429e9d
Angélica Albuquerque <angelica.o.albuquerque@gmail.com>
2023-01-10 22:07:31 -0300

    Operadores relacionais - prática

---
+++ 02-operadores/b-operadores-relacionais-pratica-1.js
@@ -0,0 +1,23 @@
+//valores literais
+console.log(7 > 9);
+console.log(7 >= 7);
+console.log(8 <= 2);
+console.log(8 < 45);
+console.log(9 == 3);
+console.log(14 != 5);
+console.log(9 != "9");
+console.log(9 !== "9");
+console.log("" == 0);
+console.log(" " == 0);
+
+//variáveis
+let a = 7;
+let b = 32;
+console.log(a > b);
+console.log(a >= b);
+console.log(a < b);
+console.log(a <= b);
+console.log(a == b);
+console.log(a != b);
+console.log(a === b);
+console.log(a !== b);
