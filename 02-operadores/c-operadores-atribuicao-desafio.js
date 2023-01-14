5df700e6fc8a45b98a7484c5b6467a284da57e67
Angélica Albuquerque <angelica.o.albuquerque@gmail.com>
2023-01-13 22:58:21 -0300

    Desafio Operadores de Atribuição

---
+++ 02-operadores/c-operadores-atribuicao-desafio.js
@@ -0,0 +1,22 @@
+/* DESAFIO - Montar uma lista de aprovados seguindo o modelo abaixo: 
+
+Lista de Aprovados
+==================
+
+1. Ana Silva
+2. Pedro Albuquerque
+3. Guilherme Pereira
+*/
+
+/*
+Exemplo de como colocar uma linha dentro de um texto:
+let texto = "Bom dia! \nTudo bem?";
+console.log(texto);
+*/
+
+let listaAprovados = "Lista de Aprovados\n";
+listaAprovados += "================== \n \n";
+listaAprovados += "1. Ana Silva \n";
+listaAprovados += "2. Pedro Albuquerque \n";
+listaAprovados += "3. Guilherme Pereira \n";
+console.log(listaAprovados);
