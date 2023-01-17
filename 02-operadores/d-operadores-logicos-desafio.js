89d93ba56a1967fc2b0da419386d400f8bd24f56
Angélica Albuquerque <angelica.o.albuquerque@gmail.com>
2023-01-16 22:39:14 -0300

    Operadores lógicos - desafio

---
+++ 02-operadores/d-operadores-logicos-desafio.js
@@ -0,0 +1,19 @@
+/*
+Criar duas expressões que resultam em TRUE e usar operadores aritméticos, relacionais e lógicos
+
+Criar duas expressões que resultam em FALSE e usar operadores aritméticos, relacionais e lógicos
+ */
+
+let verdadeiro1 = 0 <= 4 && 16 * 2 > 3;
+let verdadeiro2 = !(6 * 6 < 3);
+
+console.log(verdadeiro1);
+console.log(verdadeiro2);
+
+console.log("==============================");
+
+let falso1 = !!!true;
+let falso2 = 14000 === "14000";
+
+console.log(falso1);
+console.log(falso2);
