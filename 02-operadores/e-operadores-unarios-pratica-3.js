1e491e026e680e8e0668687ff1736036dfa1022c
Angélica Albuquerque <angelica.o.albuquerque@gmail.com>
2023-01-17 22:46:30 -0300

    Operadores unários - prática 3

---
+++ 02-operadores/e-operadores-unarios-pratica-3.js
@@ -0,0 +1,12 @@
+console.log(!!true);
+console.log(!!!false);
+
+//qualquer string que seja diferente de vazia será verdadeira
+console.log(!!"texto");
+console.log(!!"");
+
+//todos os números são convertidos para verdadeiro, com exceção do número 0
+console.log(!!1);
+console.log(!!0);
+console.log(!!-1);
+console.log(!!-0.001);
