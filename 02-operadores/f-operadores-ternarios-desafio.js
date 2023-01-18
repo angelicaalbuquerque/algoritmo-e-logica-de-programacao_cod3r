e478a69991f0f7fe6ae2d2419bb5e42ff5e2a06a
Angélica Albuquerque <angelica.o.albuquerque@gmail.com>
2023-01-17 23:34:48 -0300

    Operador ternário - desafio

---
+++ 02-operadores/f-operadores-ternarios-desafio.js
@@ -0,0 +1,16 @@
+//Fazer uma operação dependendo do operador
+
+const a = 8;
+const b = 4;
+const operacao = "/"; // + - * /
+
+const resultado =
+  operacao === "+"
+    ? a + b
+    : operacao === "-"
+    ? a - b
+    : operacao === "*"
+    ? a * b
+    : a / b;
+
+console.log(resultado);
