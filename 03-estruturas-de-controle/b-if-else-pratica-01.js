b21010a46d0a379b9e0518fa11e41c985768f5ef
Angélica Albuquerque <angelica.o.albuquerque@gmail.com>
2023-01-22 21:57:21 -0300

    If/else prática

---
+++ 03-estruturas-de-controle/b-if-else-pratica-01.js
@@ -0,0 +1,8 @@
+const verdadeira = true;
+const falsa = false;
+
+if (verdadeira) console.log("Executando");
+else console.log("Não executando");
+
+if (falsa) console.log("Não executando");
+else console.log("Executando");
