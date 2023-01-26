57fbf63eb951ce8a43f5a004691132764cb3bdc7
Angélica Albuquerque <angelica.o.albuquerque@gmail.com>
2023-01-25 22:44:32 -0300

    For - prática 04

--- 03-estruturas-de-controle/d-for-pratica-04.js
+++ 03-estruturas-de-controle/d-for-pratica-04.js
@@ -11,3 +11,19 @@ for (let i = 1; i <= 3; i++) {
 
   console.log(linha);
 }
+
+//[1, 2], [1, 3]
+//[2, 1], [2, 3]
+//[3, 1], [3, 2]
+
+for (let i = 1; i <= 3; i++) {
+  let linha = "";
+
+  for (let j = 1; j <= 3; j++) {
+    if (i !== j) {
+      linha += `[${i}, ${j}] `;
+    }
+  }
+
+  console.log(linha);
+}
