826da24735112b5dc4b458713e380f012da3a40f
Angélica Albuquerque <angelica.o.albuquerque@gmail.com>
2023-01-25 22:52:08 -0300

    Desafio - For

---
+++ 03-estruturas-de-controle/d-for-desafio.js
@@ -0,0 +1,22 @@
+/*
+exiba no console esta estrutura de cerquilhas:
+  #
+  ##
+  ###
+  ####
+  #####
+*/
+
+/* 
+let linha = "";
+
+for (let i = 1; i <= 6; i++) {
+  linha += "#";
+  console.log(linha);
+}
+*/
+
+//resolvendo sem números e sem foreach:
+for (let s = "#"; s != "#######"; s += "#") {
+  console.log(s);
+}
