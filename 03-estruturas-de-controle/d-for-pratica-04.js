a28b1764c2dfd8c62a08f286d5eb88661de03e22
Angélica Albuquerque <angelica.o.albuquerque@gmail.com>
2023-01-25 22:42:09 -0300

    Prática For (matriz)

--- 03-estruturas-de-controle/d-for-pratica-04.js
+++ 03-estruturas-de-controle/d-for-pratica-04.js
@@ -3,9 +3,11 @@
 //[3, 1], [3, 2], [3, 3]
 
 for (let i = 1; i <= 3; i++) {
-  console.log(i);
+  let linha = "";
 
   for (let j = 1; j <= 3; j++) {
-    console.log(j);
+    linha += `[${i}, ${j}] `;
   }
+
+  console.log(linha);
 }
