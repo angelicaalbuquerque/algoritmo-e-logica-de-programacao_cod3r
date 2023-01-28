9b45aa41b825bc7520e2b3b2cb2cdcbe55121a2d
Angélica Albuquerque <angelica.o.albuquerque@gmail.com>
2023-01-28 19:34:42 -0300

    Exemplo com função

---
+++ 04-funcoes/b-exemplo-com-funcao-1.js
@@ -0,0 +1,20 @@
+const d1 = 3;
+const m1 = 7;
+const a1 = 2021;
+
+function exibirData1() {
+  console.log(`Data: ${d1}/${m1}/${a1}`);
+}
+
+exibirData1();
+
+console.log("----------------");
+
+const d2 = 5;
+const m2 = 8;
+const a2 = 2022;
+
+function exibirData2() {
+  console.log(`Data: ${d2}-${m2}-${a2}`);
+}
+exibirData2();
