bc22e3f228fe60a8fe5e6893f5ec3d79ba7a48b3
Angélica Albuquerque <angelica.o.albuquerque@gmail.com>
2023-01-30 22:18:47 -0300

    Básico sobre arrays

---
+++ 05-arrays/a-basico-sobre-array-1.js
@@ -0,0 +1,6 @@
+const notas = [7.8, 6.7, 10, 9.5, 7.8, 5.8];
+console.log(notas);
+console.log(notas[0], notas[3]);
+
+notas[4] = 8.7;
+console.log(notas);
---
+++ 05-arrays/a-basico-sobre-array-2.js
@@ -0,0 +1,11 @@
+const nomes = [];
+console.log(nomes);
+
+nomes[0] = "Leonardo";
+nomes[1] = "Antony";
+nomes[2] = "Adaíla";
+nomes[3] = "Raphael";
+nomes[4] = "Milene";
+nomes[10] = "Paulo";
+
+console.log(nomes);
