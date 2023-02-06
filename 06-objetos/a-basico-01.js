e2f3cd9513a646473336823c30b619e27eeb26ef
Angélica Albuquerque <angelica.o.albuquerque@gmail.com>
2023-02-05 22:49:22 -0300

    Básico sobre objetos #1

---
+++ 06-objetos/a-basico-01.js
@@ -0,0 +1,16 @@
+let y = [];
+console.log(Array.isArray(y));
+
+let z = {};
+console.log(typeof z);
+
+const numeros = [10, 20, 30];
+console.log(numeros[0]);
+
+const produto = {
+  nome: "Caneta",
+  preco: 2.5,
+};
+
+console.log(produto.nome); //forma mais comum de acessar
+console.log(produto["preco"]);
